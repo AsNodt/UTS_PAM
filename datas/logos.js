255d6b8651e9212136b51b7c0cb1fbd9aee992e1
Daffa Sandri Ramadhan <93307988+AsNodt@users.noreply.github.com>
2023-03-22 21:58:54 +0700

    Delete logos.js

--- datas/logos.js
+++
@@ -1,9 +0,0 @@
-const logoMaskapai = {
-    citilink: require('./logoPNG/citilink.png'),
-    lion: require('./logoPNG/lion.png'),
-    batik: require('./logoPNG/batik.png'),
-    sriwijaya: require('./logoPNG/Sriwijaya.png'),
-    airasia: require('./logoPNG/AirAsia.png')
-}
-
-export default logoMaskapai;
